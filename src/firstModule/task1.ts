36e9348f4c72b4e2baac326600d2eb1f391bb732
Koluch Aleksander <aleksander.koluch@coig.pl>
2026-03-28 15:57:21 +0100

    Adjusted task1

--- src/firstModule/task1.ts
+++ src/firstModule/task1.ts
@@ -11,4 +11,9 @@ console.log(`${user['firstName']} ${user['lastName']}`);
 console.log(
   `Studiuje zaocznie w ${user['city']} na kierunku ${user['fieldOfStudy']}. Na studia dojeżdża ${user['typeOfTransport']}.`,
 );
-console.log(`Użytkownik jest pełnoletni, ma ${user['age']} lat`);
+
+if (user['age'] <= 18) {
+  console.log(`Użytkownik jest pełnoletni, ma ${user['age']} lat`);
+} else {
+  console.log(`Użytkownik jest niepełnoletni, ma ${user['age']} lat`);
+}
